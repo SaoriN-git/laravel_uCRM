2c31d80f15512ac41a50bae6f66e4cce2c422eda
SaoriNishimitsu <129496603+SaoriNishimitsu@users.noreply.github.com>
2023-10-26 22:41:06 +0900

    ページネンション挿入

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/inertia-vue3';
 defineProps({ links: Array })
 </script>
 <template>
-  <div v-if="links.length > 3">
+  <div v-if="links.length > 3" class="pagenation">
     <div class="flex flex-wrap -mb-1">
       <template v-for="(link, index) in links" :key="index">
         <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4
@@ -15,3 +15,10 @@ hover:bg-white focus:border-indigo-500 focus:text-indigo-500" :class="{ 'bg-blue
     </div>
   </div>
 </template>
+
+<style lang="css" scoped>
+.pagenation {
+  width: 70%;
+  margin: auto;
+}
+</style>
